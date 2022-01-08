ffd5f32b08373eb68f3a4a468242295e7355b439
marwa234 <54775927+marwa234@users.noreply.github.com>
2022-01-08 18:37:27 +0200

    Delete js directory

--- js/getcarddata.js
+++
@@ -1,9 +0,0 @@
-var pImg =document.getElementById('pImg');
-var pTitle= document.getElementById('pTitle');
-var pPrice =document.getElementById('pPrice');
-var pDescri=document.getElementById('pDescri');
-
-pPrice.innerHTML=localStorage.price;
-pTitle.innerHTML=localStorage.title;
-pDescri.innerHTML=localStorage.description;
-pImg.src=localStorage.image;
